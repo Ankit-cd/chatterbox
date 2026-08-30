fff728aff96095a3242a9a47f6808c23848a4221
Ankit-cd <ankitmehta4526@gmail.com>
2026-08-30 22:14:05 +0530

    next-auth

--- app/api/auth/(...nextauth)/route.ts
+++ app/api/auth/(...nextauth)/route.ts
@@ -1,25 +1,74 @@
-import bcrypt from  'bcrypt';
-import NextAuth, {AuthOptions} from 'next-auth';
+import bcrypt from "bcrypt";
+import NextAuth, { AuthOptions } from "next-auth";
 
-import Credentials from 'next-auth/providers/credentials';
-import Github from 'next-auth/providers/github';
-import Google from 'next-auth/providers/google';
-import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import Credentials from "next-auth/providers/credentials";
+import Github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
-import prisma from '@/app/libs/prismadb';
+import prisma from "@/app/libs/prismadb";
+
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    Github({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+
+    Credentials({
+      name: "credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "text",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+        },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Email and password are required");
+        }
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+
+        if (!user || !user.hashedPassword) {
+          throw new Error("Invalid email or password");
+        }
+
+        const isCorrectPassword = await bcrypt.compare(
+          credentials.password,
+          user.hashedPassword,
+        );
+
+        if (!isCorrectPassword) {
+          throw new Error("Invalid email or password");
+        }
+
+        return user;
+      },
+    }),
+  ],
+
+  debug:process.env.NODE_ENV === 'development',
+  session:{
+    strategy:'jwt',
+  },
+  secret:process.env.NEXTAUTH_SECRET,
+};
 
 
-export const authOptions:AuthOptions ={
-    adapter:PrismaAdapter(prisma),
-    providers:[
-        Github({
-            clientId:process.env.GITHUB_ID as string,
-            clientSecret:process.env.GITHUB_SECRET as string,
-        }),
-         Google({
-            clientId:process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret:process.env.GOOGLE_CLIENT_SECRET as string,
-        }),
 
-    ]
-}
+const handler = NextAuth(authOptions);
+
